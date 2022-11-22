d8fdf442fe22c03071cb88f8e32b19bf3d6762da
Harry Escasinas <115538908+EscasinasHarry@users.noreply.github.com>
2022-11-23 00:54:51 +0800

    Update Exercise1.m

--- Exercise1.m
+++ Exercise1.m
@@ -8,7 +8,7 @@ fruits = imread('C:/Users/Harry/Desktop/School 2022/Elective Course/Activity 1/f
 imshow(fruits);
 imfinfo 'fruits.png'
 
-%%re size the image into blurry version and save it as fruits2.png
+%re size the image into blurry version and save it as fruits2.png
 RE_size = imresize(fruits, 1/17);
 imwrite(RE_size, 'C:/Users/Harry/Desktop/School 2022/Elective Course/Activity 1/fruits2.png');
 figure(2), imshow(RE_size);
